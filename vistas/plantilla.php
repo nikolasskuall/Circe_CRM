1623eced02c67c23ddff9581e12170becc59fedd
nikolasskuall <nikolas.skuall@gmail.com>
2022-01-04 20:13:13 -0500

    maquetado

---
+++ vistas/plantilla.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Circe CRM</title>
+           <!-- Theme Grip-->
+   <link rel="stylesheet" href="inc/css/silver-grip.css">
+</head>
+<body>
+    <section class="tester cat-row">
+        <aside class="bar-lateral">
+             Logo
+        </aside>
+        <section class="main-conten">
+            <header></header>
+
+        </section>
+    </section>
+</body>
+</html>
